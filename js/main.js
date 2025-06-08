e75b59e93e584fe656fc0fbe044ecc44c3796ed3
borii1203 <1minji@naver.com>
2025-06-08 22:15:15 +0900

    20250608

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(function() {
-    $('.header').load('common/header.html')
+    $('.header').load('../common/header.html')
+    $('.footer').load('../common/footer.html')
+
+    var swiper = new Swiper(".mainSwiper", {
+                loop: true,
+                effect: "fade",
+                pagination: {
+                    el: ".swiper-pagination",
+                },
+            });
+    
 })

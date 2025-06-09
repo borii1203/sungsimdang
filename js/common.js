fe4a106bffd7074d9e2824b4cad43a32aa297e79
borii1203 <1minji@naver.com>
2025-06-09 17:35:58 +0900

    20250609

--- js/common.js
+++ js/common.js
@@ -1,4 +1,33 @@
 $(function () {
+    let $topBtn = $('.top_btn');
+    let section01H = $('.section_01').offset().top;
+    let footerH = $('.footer').offset().top;
+    let windowH = $(window).height();
+
+    //탑버튼
+    $topBtn.on('click', function () {
+        $('html').animate({
+            scrollTop: 0
+        }, 1000)
+    });
+
+    $(window).on('scroll', function () {
+        let scrollbar = $(this).scrollTop();
+
+        if (scrollbar > section01H) {
+            $topBtn.addClass('show');
+        } else {
+            $topBtn.removeClass('show');
+        }
+
+        if (scrollbar + windowH > footerH) {
+            $topBtn.addClass('color_wh');
+        } else {
+            $topBtn.removeClass('color_wh');
+        }
+    })
+
+
     let navHeight = $('.depth_02').height() + 32;
 
     $('.nav_bg').css('height', navHeight)

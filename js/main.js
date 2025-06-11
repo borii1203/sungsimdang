cb88b2bbb82052ea3dc812587a3d9fab3a82c38c
borii1203 <1minji@naver.com>
2025-06-11 15:07:24 +0900

    20250611

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
-$(function() {
+$(function () {
+    AOS.init();
     $('.header').load('common/header.html')
     $('.footer').load('common/footer.html')
 
     var swiper = new Swiper(".mainSwiper", {
-                loop: true,
-                effect: "fade",
-                pagination: {
-                    el: ".swiper-pagination",
-                },
-            });
-    
+        loop: true,
+        speed:1500,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        effect: "fade",
+        pagination: {
+            el: ".swiper-pagination",
+        },
+    });
+
+    $('#popup .close_btn').on('click', function() {
+        $('#popup').hide();
+    });//
+
 })
